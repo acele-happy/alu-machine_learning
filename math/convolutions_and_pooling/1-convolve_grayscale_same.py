23500e15933f43f9ab8179a663bd11c657574746
Dukunde Irakoze Happy <acele4444@gmail.com>
2026-07-20 15:48:54 +0200

    same

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Performs a same convolution on grayscale images."""
+"""Same convolution on grayscale images."""
 import numpy as np
 
 
@@ -7,38 +7,29 @@ def convolve_grayscale_same(images, kernel):
     """
     Performs a same convolution on grayscale images.
 
-    images: numpy.ndarray of shape (m, h, w)
-        m is the number of images
-        h is the height in pixels of the images
-        w is the width in pixels of the images
-    kernel: numpy.ndarray of shape (kh, kw)
-        kh is the height of the kernel
-        kw is the width of the kernel
+    Args:
+        images: numpy.ndarray of shape (m, h, w)
+        kernel: numpy.ndarray of shape (kh, kw)
 
-    If necessary, the image is padded with 0's.
-
-    Returns: numpy.ndarray containing the convolved images
+    Returns:
+        numpy.ndarray containing the convolved images
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-
-    pad_h_top = (kh - 1) // 2
-    pad_h_bottom = kh // 2
-    pad_w_left = (kw - 1) // 2
-    pad_w_right = kw // 2
-
-    padded_images = np.pad(
+    pad_h = kh // 2
+    pad_w = kw // 2
+    padded = np.pad(
         images,
-        pad_width=((0, 0), (pad_h_top, pad_h_bottom), (pad_w_left, pad_w_right)),
-        mode='constant',
-        constant_values=0
+        ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+        mode='constant'
     )
-
-    convolved = np.zeros((m, h, w))
+    output = np.zeros((m, h, w))
 
     for i in range(h):
         for j in range(w):
-            image_slice = padded_images[:, i:i + kh, j:j + kw]
-            convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+            output[:, i, j] = np.sum(
+                padded[:, i:i + kh, j:j + kw] * kernel,
+                axis=(1, 2)
+            )
 
-    return convolved
+    return output
